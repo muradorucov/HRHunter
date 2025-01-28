508210093f40822c388f8d589eee59777773a2dd
muradorucov <orucowmurad@gmail.com>
2025-01-28 06:41:57 +0400

    feat:js intro

---
+++ Lesson23/main.js
@@ -0,0 +1,124 @@
+// 'use stirct'
+
+// console.log();
+// document.querySelector("button").addEventListener("click",()=>{
+
+// })
+
+// console.log("Salam");
+
+
+
+// input & output
+// variables
+// data types
+// operators
+// contidions
+// loops
+// functions
+// object
+// array
+
+
+// console.log("Bu bir mesajdir")
+// console.error("Bu bir mesajdir")
+// console.warn("Bu bir mesajdir")
+// document.write("Bu bir mesajdir")
+// alert("Bu bir mesajdir")
+
+
+// confirm("Blogu silmek isteyirsinizmi?");
+
+
+
+// let age = 30;
+// age = 50
+// const
+// var
+// const age = 25
+// age = 50
+// age = 34;
+// var age = 46;
+// age = 56
+
+
+// console.log("Salam ",username);
+// console.log("Xos gelmisen ",username);
+// console.log("Necesen ",username);
+
+
+
+// Primitve
+// Non-Primitive
+
+
+
+
+// let username = "murad";
+// username = "test";
+
+
+// let fakhri = "";
+// fakhri = "Fakhri";
+
+
+
+
+// console.log(prompt());
+
+
+// Data types
+// Primitive
+// String => "metn" "56" "ture" 'false' "salam necsen"
+// Number => 56 34.78 -10 0 1
+// NaN
+// infinity
+
+// Boolean => true false
+// Null
+// Undefined
+
+// Bigint
+// Symbol
+
+// Non-Primitive
+
+// let age = {};
+// console.log(age);
+
+
+
+
+// NOn-Primitive:
+// Object
+// => Object
+// => Array
+// => Function
+// => Date
+// => RegExp
+// => Set
+// => Map
+// => WeakMap
+
+
+
+let fullName = "Example Example";
+const age = 34;
+
+let text = "Salam " + fullName + " xosh gelmisiniz. Sizin yasiniz " + age;
+
+console.log(text);
+
+
+
+
+
+// snake case
+// let $_u3545s_er$_name_$028638 = ""
+// // camel
+// let class = ""
+// // kebab
+// let UserName = ""
+
+
+
